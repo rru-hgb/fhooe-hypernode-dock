4b6a3759a9f72ca3f3a5d8ae9db7563d41d3064c
Rimbert Rudisch-Sommer <rimbert.rudisch-sommer@fh-hagenberg.at>
2022-11-22 13:46:50 +0100

    MEVN Setup

--- CleanReinstall.sh
+++ CleanReinstall.sh
@@ -7,7 +7,7 @@ echo "Remove all unused images, containers, networks and volumes"
 docker system prune --volumes --all --force
 
 echo "Update fhooe-web-dock from GitHub"
-git pull
+#git pull
 
 echo """Create and start the containers again in the background (detached)"
 docker compose up --detach
---
+++ node/README.md
@@ -0,0 +1,4 @@
+# node folder mapped to Docker Container
+## This content will visible in the Container after mounting as an docker volume
+
+Add your Node code here
