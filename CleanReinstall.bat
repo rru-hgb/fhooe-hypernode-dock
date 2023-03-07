47d21f65a0a7a93d9c68a9b8d2032453a55e4e52
Rimbert Rudisch-Sommer <rru-hgb@users.noreply.github.com>
2023-03-07 05:32:32 +0100

    fix name

--- CleanReinstall.bat
+++ CleanReinstall.bat
@@ -6,11 +6,11 @@ docker compose down --volumes
 echo Remove all unused images, containers, networks and volumes
 docker system prune --volumes --all --force
 
-echo Update fhooe-web-dock from GitHub
+echo Update fhooe-onm-dock from GitHub
 git pull
 
 echo Create and start the containers again in the background (detached)
 docker compose up --detach
 
-echo All finished. Enjoy your updated version of fhooe-web-dock!
+echo All finished. Enjoy your updated version of fhooe-onm-dock!
 pause
